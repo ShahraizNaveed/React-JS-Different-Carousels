0395b18e36ece7ac94010bfaa0aa723ce3d413d2
mshahzaib913 <mianshahzaibnaveed@gmail.com>
2023-05-18 01:07:35 +0500

    Complete Carousel Code

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import Carousel from "./Carousel";
+import MultiItemCarousel from './MultiItemCrousel';
+import BootstrapMultiCarousel from "./BootstrapMulti";
+import Testimonial from "./Testimonial";
+import Ipl from './Ipl';
+
+function App() {
+  return (
+    <div className='App'>
+      <Carousel />
+      <MultiItemCarousel />
+      <Ipl />
+      <BootstrapMultiCarousel />
+      <Testimonial />
+    </div>
+  );
+}
+
+export default App;
